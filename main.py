c614817bed7da697706f1d6aa8201ebba7bc2179
Arnaud CASSEVILLE <contact.pro.user00@gmail.com>
2024-08-02 15:45:15 +0200

    add main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+import requests
+import csv
+from bs4 import BeautifulSoup
+import extract 
+import tool_box as tb
+
+base_url = 'https://books.toscrape.com/'
+
+def main():
+
+    input_data = input('Nombre de catégories a scrap : ')
+    print(input_data)
+    content = tb.get_html_content(base_url)
+    data_url_category, data_title_category = extract.extract_url_category(content, base_url)
+    extract.iterate_category(data_url_category, data_title_category, input_data)
+
+if __name__== "__main__":
+    main()
