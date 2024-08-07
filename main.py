9afe99a58c56349feec02bd6ead57a7adb85c04b
Arnaud CASSEVILLE <contact.pro.user00@gmail.com>
2024-08-07 15:40:27 +0200

    Add a parameter for scrap a number of category given with input

--- main.py
+++ main.py
@@ -7,9 +7,7 @@ import tool_box as tb
 base_url = 'https://books.toscrape.com/'
 
 def main():
-
-    input_data = input('Nombre de catégories a scrap : ')
-    print(input_data)
+    input_data = tb.get_input_user()
     content = tb.get_html_content(base_url)
     data_url_category, data_title_category = extract.extract_url_category(content, base_url)
     extract.iterate_category(data_url_category, data_title_category, input_data)
